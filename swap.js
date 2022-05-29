77c5154ba1f2f4344ec5982af54094e610cfad7e
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2022-05-29 21:17:11 +0600

    swap a variable

---
+++ swap.js
@@ -0,0 +1,39 @@
+//swap holo kono kichu change kora, man 1tar value onnotay jaoya
+
+var a = 5;
+var b = 7;
+console.log("before swap: a =", a, "b =", b);
+
+// mone koro tmr 2ta bati ase
+// 1st bati te jus
+// 2nd bati te pani
+
+// akhn bati change korte chaile
+// onno 1ta bati nite hobe & sei bati te age jus rakhte hobe
+
+// then 1st bati khali hole, other batir pani 1st bati te dhalte hobe
+
+var temp = a;
+//temp variable ta holo, sei boro bati, a er jus temp a rakhlam
+a = b;
+//b er pani 1st bati te rakhlam
+b = temp;
+//temp a rakha jus 2nd bati te nilam
+console.log("after swap: a =", a, "b =", b);
+
+// 2nd method
+
+var x = 5;
+var y = 7;
+x = x + y;
+y = x - y;
+x = x - y;
+console.log("after swap: x =", x, "y =", y);
+
+// 3rd method, ata sudhui javascript a kaj korbe
+
+var p = 5; 
+var q = 7;
+
+[p, q] = [q, p]
+console.log("after swap: p =", p, "q =", q);
