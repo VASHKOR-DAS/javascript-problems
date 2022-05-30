e236e13c9feee54c6fc39e1cc2968054a458a22e
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2022-05-31 00:29:27 +0600

    own way added

--- removeDuplicateFromArray.js
+++ removeDuplicateFromArray.js
@@ -1,3 +1,5 @@
+/**
+
 var name = [3, 6, 2, 7, 3, 2, 8, 1, 9, 11, 56];
 //bachai kore niche rakhbo
 var uniqueName = [];
@@ -12,3 +14,18 @@ for(var i = 0; i < name.length; i++){
     }
 }
 console.log("uniqueName :", uniqueName);
+
+*/
+
+
+//own way
+
+var name = [3, 6, 2, 7, 3, 2, 8, 1, 9, 11, 56];
+uniqueName = [];
+
+for(var i = 0; i < name.length; i++){
+    if(uniqueName.indexOf(name[i])== -1){
+        uniqueName.push(name[i]);
+    }
+}
+console.log(uniqueName);
