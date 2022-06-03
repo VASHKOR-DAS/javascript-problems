3f0caaf32bac5bfaf05d1996286442ccf6d8c2ed
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2022-06-03 15:17:40 +0600

    assingment

---
+++ assignment.js
@@ -0,0 +1,43 @@
+/**
+feetToMile covert
+
+
+woodCalculator
+
+chair 1qb feel wood
+table 3qb feel wood
+khat 5qb feel wood
+
+
+
+brickCalculator
+
+1 - 10tola = 15feet
+11 - 20tola = 12feet
+21 tola theke up = 10feel
+every feet a hight nned a brick 1000pcs
+
+
+
+tinyFriend
+sobcheye choto frnd er nam array er vitor theke khuje ber korte hobe
+*/
+
+
+//assinment 1
+
+function feetToMile(feet) {
+    var feet = feet*0.0001893939;
+    return feet;
+}
+
+var feet = feetToMile(5280);
+console.log(feet);
+
+
+
+// assignment 2
+
+function woodCalculator(result) {
+    
+}
